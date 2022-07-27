a06d3a9befb418365b5a73a0d966634b634a5686
vedangi163 <59529326+vedangi163@users.noreply.github.com>
2022-07-27 17:56:12 +0530

    Add files via upload

---
+++ FinalClassTest.java
@@ -0,0 +1,40 @@
+
+public class FinalClassTest {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
+
+class PersonClass
+{
+	final String aadharNum="1234 5236 9875";
+	
+	final void getAadharCard()
+	{
+		
+	}
+}
+class StudentClass extends PersonClass
+{
+	void getAadhar()
+	{
+		
+	}
+}
+class Shape
+{
+	
+}
+final class Triangle extends Shape
+{
+	
+}
+//If we extend Circle class it will give an error, because Triangle class is final 
+
+//class Circle extends Triangle 
+//{
+//	
+//}
